e903c6b8e9ee25c9dc8007d3816bf66057ee2f94
Karim O <karimortiz007@gmail.com>
2025-10-08 15:23:46 -0700

    Actualizado(ahora si corre)

--- BigFourApp/Program.cs
+++ BigFourApp/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<BaseDatos>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+//builder.Services.AddDbContext<BaseDatos>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
