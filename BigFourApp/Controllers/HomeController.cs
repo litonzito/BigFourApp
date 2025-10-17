be279043994461db5a325a3889cb761861ec0673
Megakinn <165713984+Megakinn@users.noreply.github.com>
2025-10-17 14:43:21 -0700

    Added Event Listings and View details

--- BigFourApp/Controllers/HomeController.cs
+++ BigFourApp/Controllers/HomeController.cs
@@ -1,27 +1,34 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BigFourApp.Models;
+using BigFourApp.Persistence;
+using Microsoft.EntityFrameworkCore;
+using BigFourApp.Models.Event;
 
 namespace BigFourApp.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly BaseDatos _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, BaseDatos context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var eventos = _context.Events
+            .Include(e => e.Images)
+            .Include(e => e.Venues)
+            .Include(e => e.Classifications)
+            .OrderBy(e => e.Date)
+            .ToList();
+        return View(eventos);
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
 
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
