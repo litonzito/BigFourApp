653eeb8f1aacb6672f252ee396fafeeb81adec44
Karim O <karimortiz007@gmail.com>
2025-11-19 11:59:11 -0800

    Cambio de usuario id tipo int a string para alinearse con Identity

--- BigFourApp/Models/UsuariosOLD.cs
+++ BigFourApp/Models/UsuariosOLD.cs
@@ -6,7 +6,7 @@ namespace BigFourApp.Models
 {
     public class Usuario
     {
-        public int Id_Usuario { get; set; }
+        public string Id_Usuario { get; set; }
         public int ventaId { get; set; }
         [Required]
         public string Nombre { get; set; } = null!;
